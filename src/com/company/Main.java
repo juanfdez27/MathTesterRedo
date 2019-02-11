09db3dbe0220e3b81b516dc244bbb314d003394d
juan <juan.fernandez.doremire.2020@student.lhschools.org>
2019-02-11 07:45:08 -0500

    Introduction of my brother and I

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,10 +9,15 @@ public class Main {
         int firstNumber;
         int secondNumber;
 
-        firstWord = "play";
-        secondWord = "house";
+        firstWord = "Juan";
+        secondWord = "Fernandez";
         firstNumber = 2;
         secondNumber = 4;
-        
+
+
+        System.out.println(firstWord+secondWord);
+        System.out.println("My name is "+firstWord+secondWord+", I am "+secondNumber*secondNumber+" years old");
+        System.out.println("I have a little brother, he is in "+(firstNumber+secondNumber)+"th grade");
+
     }
 }
