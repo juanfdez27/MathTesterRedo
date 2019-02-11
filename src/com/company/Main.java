fc71d70d77b7ff6ad673ba4f0104e1701cf66913
juan <juan.fernandez.doremire.2020@student.lhschools.org>
2019-02-11 07:50:44 -0500

    Introduced my sister and how she was late to school

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,8 +16,11 @@ public class Main {
 
 
         System.out.println(firstWord+secondWord);
-        System.out.println("My name is "+firstWord+secondWord+", I am "+secondNumber*secondNumber+" years old");
-        System.out.println("I have a little brother, he is in "+(firstNumber+secondNumber)+"th grade");
+        System.out.println("My name is "+firstWord+secondWord+", I am "+secondNumber*secondNumber+" years old.");
+        System.out.println("I have a little brother, he is in "+(firstNumber+secondNumber)+"th grade.");
+        System.out.println("I also have a little sister, she is in "+(secondNumber-firstNumber)+"nd grade.");
+        System.out.println("If I do not not take my sister to school on time she will get a "+(secondNumber%firstNumber)+" on her test.");
+        System.out.println("Her teacher excused her even though she was "+secondNumber/firstNumber+" minutes late.");
 
     }
 }
